6ad8622dc8315a60b100a5def1c4b54c0f1d38e9
Animesh Risal <animeshrisal99@gmail.com>
2024-11-05 08:42:17 +0100

    Add main page

---
+++ app/blog/adding-virtio-to-your-risc-v-os/Page.tsx
@@ -0,0 +1,5 @@
+export default function Post() {
+  return (
+    <div> Work in progress!</div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <div className={styles.page}>
-
+    <div>
+      <h1>Animesh Risal.</h1>
+      <div>
+        Contact: emugrid@proton.me
+      </div>
+      <p>Hi. Welcome to my website. You can find some of my info here</p>
+      <p>Some of my interests.
+        Emulation, Virtualization, Retro Games, Cloud Computing, RISC-V
+      </p>
+      <br />
+      <div>
+        <h1>About me</h1>
+        <br />
+        <p>
+          I&lsquo;m currently a Masters student studying International Information System Science at Otto-Friedrich-Universität Bamberg. I also work part time for Lion5 GmbH. I have 5 years of professional experience working as a full stack web developer.
+        </p>
+        <p>An area of interest I&lsquo;ve picked up over the last few months is RISC-V and 6502 Emulation. </p>
+      </div>
     </div>
   );
 }
